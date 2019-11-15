dbf24f3b27af3d5b584327808ada5107a98516f8
Guilherme Brito <guilhermebritto.prof@gmail.com>
2019-11-15 17:54:34 -0200

    Correcao de cors e adicionar de script para startar a aplicao

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import routes from "./routes";
 import cors from "cors";
+
 class App {
   constructor() {
     this.server = express();
@@ -11,7 +12,7 @@ class App {
 
   middlewares() {
     this.server.use(express.json());
-    this.server.user(cors());
+    this.server.use(cors());
   }
 
   routes() {
